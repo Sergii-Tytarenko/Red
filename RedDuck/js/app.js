240ec5584d63197717fee0ffd2b5bf3647c45997
Sergii-Tytarenko <tytarenko.sergii@gmail.com>
2021-02-08 17:32:00 +0200

    update smooth scroll function

--- RedDuck/js/app.js
+++ RedDuck/js/app.js
@@ -163,22 +163,29 @@ function body_lock_add(delay) {
 
 /* Smoth scroll
 -----------------------------------------------------------------------------*/
-const smoothLinks = document.querySelectorAll('.smooth');
-
-if (smoothLinks.length > 0) {
-	for (let smoothLink of smoothLinks) {
-		smoothLink.addEventListener('click', function (e) {
-			e.preventDefault();
-			const id = smoothLink.getAttribute('href');
-	
-			document.querySelector(id).scrollIntoView({
-				behavior: 'smooth',
-				block: 'start'
-			});
-		});
-	};
+const linkNav = document.querySelectorAll('.smooth'),
+      V = .15; 
+for (let link of linkNav) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); 
+        let w = window.pageYOffset,  
+            hash = this.href.replace(/[^#]*(.*)/, '$1');  
+        t = document.querySelector(hash).getBoundingClientRect().top,  
+            start = null;
+        requestAnimationFrame(step); 
+        function step(time) {
+            if (start === null) start = time;
+            let progress = time - start,
+                r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+            window.scrollTo(0,r);
+            if (r != w + t) {
+                requestAnimationFrame(step)
+            } else {
+                location.hash = hash  
+            }
+        }
+    }, false);
 }
-
 
 /* Modal Windows
 -----------------------------------------------------------------------------*/
